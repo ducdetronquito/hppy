89591bc7015d58c59f037a2ec6d8a4b6dfed9c99
ducdetronquito <guillaume.paulet@giome.fr>
2019-01-06 23:13:48 +0100

    :alembic: Add idea on how to handle node creation.

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -8,6 +8,17 @@ fn parse(content: &str) -> Document {
 
     for c in content.chars() {
         context.handle(c);
+
+        // Main idea on how to handle node creation.
+        //
+        // if context.node_is_pending() {
+        //     tag_name = context.get_tag_name();
+        //     content = context.get_content();
+        //     attributs = context.get_attributes();
+        //     document.add(tag_name, content, attributes);
+        // }
+        //
+        // Maybe the token_stack should be managed by the parse function and not the parsing Context.
     }
 
     return document;
