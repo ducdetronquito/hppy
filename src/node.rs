3756444fc0ad35bd945645872dd3de79798fb708
ducdetronquito <guillaume.paulet@giome.fr>
2019-01-11 00:09:22 +0100

    :recycle: The tokenizer is now responsible to return list of Tokens.

--- src/node.rs
+++ src/node.rs
@@ -1,5 +1,5 @@
 #[derive(Debug, PartialEq)]
-enum Tag {
+pub enum Tag {
     Body,
     Div,
     P,
@@ -14,38 +14,51 @@ impl Tag {
             "div" => Tag::Div,
             "p" => Tag::P,
             "" => Tag::Text,
-            _ => Tag::Undefined
+            _ => Tag::Undefined,
         }
     }
 }
 
 #[derive(Debug)]
 pub struct Node {
-    tag: Tag,
-    text_content: String,
+    pub tag: Tag,
+    pub text_content: String,
     id: i32,
 }
 
 impl Node {
-
-    pub fn new(tag_name: &str, text_content: &str) -> Self {
+    pub fn tag(tag_name: &str) -> Self {
         Node {
             tag: Tag::from_name(tag_name),
+            text_content: String::new(),
+            id: -1,
+        }
+    }
+
+    pub fn text(text_content: &str) -> Self {
+        Node {
+            tag: Tag::Text,
             text_content: text_content.to_string(),
-            id: -1
+            id: -1,
         }
     }
 }
-
 
 #[cfg(test)]
 mod tests {
     use super::{Node, Tag};
 
     #[test]
-    fn test_new_node() {
-        let node = Node::new("div", "Hello Hppy!");
+    fn test_create_a_tag_node() {
+        let node = Node::tag("div");
         assert_eq!(node.tag, Tag::Div);
+        assert_eq!(node.text_content, "")
+    }
+
+    #[test]
+    fn test_create_a_text_node() {
+        let node = Node::text("Hello Hppy!");
+        assert_eq!(node.tag, Tag::Text);
         assert_eq!(node.text_content, "Hello Hppy!")
     }
 }
