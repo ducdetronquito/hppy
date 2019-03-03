75782b122ae7c60416b83d5dd46a9f908b257d48
ducdetronquito <guillaume.paulet@giome.fr>
2019-03-03 17:54:38 +0100

    :alembic: Manage nodes scopes (but do not handle incorrect inputs).

--- src/document.rs
+++ src/document.rs
@@ -3,7 +3,7 @@ use crate::tag::{Tag};
 pub struct Document {
     pub tags: Vec<Tag>,
     pub texts: Vec<String>,
-    pub relationships: Vec<i32>,
+    pub parents: Vec<i32>,
 }
 
 impl Document {
@@ -11,18 +11,24 @@ impl Document {
         Document {
             tags: Vec::new(),
             texts: Vec::new(),
-            relationships: Vec::new(),
+            parents: Vec::new(),
         }
     }
 
-    pub fn push_tag(&mut self, tag_name: &str) {
+    pub fn push_tag(&mut self, tag_name: &str, parent_index: i32) {
         self.tags.push(Tag::from_name(tag_name));
         self.texts.push("".to_string());
+        self.parents.push(parent_index);
     }
 
-    pub fn push_text(&mut self, text: &str) {
+    pub fn push_text(&mut self, text: &str, parent_index: i32) {
         self.tags.push(Tag::Text);
         self.texts.push(text.to_string());
+        self.parents.push(parent_index);
+    }
+
+    pub fn get_last_node_index(& self) -> i32 {
+        self.tags.len() as i32
     }
 }
 
@@ -33,7 +39,7 @@ mod tests {
     #[test]
     fn test_push_tag() {
         let mut document = Document::new();
-        document.push_tag("div");
+        document.push_tag("div", -1);
         assert_eq!(document.tags.len(), 1);
         assert_eq!(document.tags[0], Tag::Div);
         assert_eq!(document.texts[0], "");
@@ -42,7 +48,7 @@ mod tests {
     #[test]
     fn test_push_text() {
         let mut document = Document::new();
-        document.push_text("Hello Hppy!");
+        document.push_text("Hello Hppy!", -1);
         assert_eq!(document.tags.len(), 1);
         assert_eq!(document.tags[0], Tag::Text);
         assert_eq!(document.texts[0], "Hello Hppy!");
--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -7,14 +7,22 @@ fn parse(content: &str) -> Document {
     let mut scopes = Vec::new();
 
     for token in Tokenizer::get_tokens(content) {
-        if token.is_text() {
-            document.push_text(&token.content);
-            continue;
+
+        if token.is_closing_tag() {
+            scopes.pop();
         }
-        if token.is_opening_tag() {
-            scopes.push(token.content.clone());
-            document.push_tag(&token.content);
-        } else if token.is_closing_tag() {
+        else {
+            let parent_index: i32 = match scopes.last() {
+                Some(x) => *x - 1,
+                None => -1
+            };
+            if token.is_text() {
+                document.push_text(&token.content, parent_index);
+            }
+            else if token.is_opening_tag() {
+                document.push_tag(&token.content, parent_index);
+                scopes.push(document.get_last_node_index());
+            }
         }
     }
 
@@ -27,11 +35,18 @@ mod tests {
 
     #[test]
     fn test_parse() {
-        let document = parse("<div>Hello Hppy</div>");
-        assert_eq!(document.tags.len(), 2);
+        let document = parse("<div><p>Hello</p>Happy</div>");
+        assert_eq!(document.tags.len(), 4);
         assert_eq!(document.tags[0], Tag::Div);
         assert_eq!(document.texts[0], "");
-        assert_eq!(document.tags[1], Tag::Text);
-        assert_eq!(document.texts[1], "Hello Hppy");
+        assert_eq!(document.tags[1], Tag::P);
+        assert_eq!(document.texts[1], "");
+        assert_eq!(document.parents[1], 0);
+        assert_eq!(document.tags[2], Tag::Text);
+        assert_eq!(document.texts[2], "Hello");
+        assert_eq!(document.parents[2], 1);
+        assert_eq!(document.tags[3], Tag::Text);
+        assert_eq!(document.texts[3], "Happy");
+        assert_eq!(document.parents[3], 0);
     }
 }
