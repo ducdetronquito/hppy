bd5f28c98c38b1ad347849a855bda384088abb19
ducdetronquito <g.paulet@zero-gachis.com>
2019-06-14 14:23:26 +0200

    :white_check_mark: Rename parser tests.

--- src/parser.zig
+++ src/parser.zig
@@ -80,7 +80,7 @@ var alloc = direct_allocator.allocator;
 
 // ----- Test Parse -----
 
-test "Parse." {
+test "Parse tag." {
     var document = try parse(&alloc, &"<div></div>");
 
     var tags = document.tags.toSlice();
@@ -89,56 +89,14 @@ test "Parse." {
     assert(parents[1] == 0);
 }
 
-test "Parse nested" {
-    var document = try parse(&alloc, &"<div><p></p></div>");
-
-    var tags = document.tags.toSlice();
-    var parents = document.parents.toSlice();
-    assert(tags[1] == Tag.Div);
-    assert(parents[1] == 0);
-    assert(tags[2] == Tag.P);
-    assert(parents[2] == 1);
-}
-
-
-test "Parse text." {
-    var document = try parse(&alloc, &"<div>Hello Hppy</div>");
-
-    var tags = document.tags.toSlice();
-    var parents = document.parents.toSlice();
-    var texts = document.texts.toSlice();
-    assert(tags[2] == Tag.Text);
-    assert(parents[2] == 1);
-    assert(Bytes.equals(texts[2], "Hello Hppy"));
-}
-
-test "By default each tag as an empty string." {
+test "Parse tag - By default each tag as an empty string." {
     var document = try parse(&alloc, &"<div></div>");
 
     var texts = document.texts.toSlice();
     assert(Bytes.equals(texts[1], ""));
 }
 
-test "Text do not create a new hierarchy." {
-    var html =
-        \\<div>
-        \\ Hello Hppy
-        \\  <p></p>
-        \\</div>
-    ;
-    var document = try parse(&alloc, &html);
-
-    var tags = document.tags.toSlice();
-    var parents = document.parents.toSlice();
-    assert(tags[1] == Tag.Div);
-    assert(tags[2] == Tag.Text);
-    assert(tags[3] == Tag.P);
-    assert(tags[parents[2]] == Tag.Div);
-    assert(tags[parents[3]] == Tag.Div);
-}
-
-
-test "Parse multiple nested" {
+test "Parse tag - with multiple nested tags." {
     var html =
         \\<div>
         \\  <p>
@@ -163,7 +121,37 @@ test "Parse multiple nested" {
 }
 
 
-test "Parse a key only attribute." {
+test "Parse text." {
+    var document = try parse(&alloc, &"<div>Hello Hppy</div>");
+
+    var tags = document.tags.toSlice();
+    var parents = document.parents.toSlice();
+    var texts = document.texts.toSlice();
+    assert(tags[2] == Tag.Text);
+    assert(parents[2] == 1);
+    assert(Bytes.equals(texts[2], "Hello Hppy"));
+}
+
+test "Parse text - Do not create a new hierarchy." {
+    var html =
+        \\<div>
+        \\ Hello Hppy
+        \\  <p></p>
+        \\</div>
+    ;
+    var document = try parse(&alloc, &html);
+
+    var tags = document.tags.toSlice();
+    var parents = document.parents.toSlice();
+    assert(tags[1] == Tag.Div);
+    assert(tags[2] == Tag.Text);
+    assert(tags[3] == Tag.P);
+    assert(tags[parents[2]] == Tag.Div);
+    assert(tags[parents[3]] == Tag.Div);
+}
+
+
+test "Parse attributes - with key-only attribute." {
     var document = try parse(&alloc, &"<div disabled >Hello</div>");
 
     var tags = document.tags.toSlice();
@@ -174,7 +162,7 @@ test "Parse a key only attribute." {
 }
 
 
-test "Key only arguments can be surrounded by any space characters." {
+test "Parse attributes - Key-only arguments can be surrounded by any space characters." {
     var document = try parse(&alloc, &"<div     disabled      >Hello</div>");
 
     var tags = document.tags.toSlice();
